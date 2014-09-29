79bfde64c8a6c800a960e13a0f67916c1ff88524
Cat Thomas <catstarrthomas@gmail.com>
2014-09-28 20:37:18 -0500

    Implemented AbstractedArray & ArrayClass

--- AbstractArray.h
+++ AbstractArray.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 using namespace std;
-#include <exception>
+#include "Exception.h"
 class ArrayException : public Exception { };
 class ArrayMemoryException : public ArrayException { }; //Array does not fit into available memory
 class ArrayBoundsException : public ArrayException { }; //Tried to access element outside of range of array
@@ -24,4 +24,20 @@ public:
 	virtual DataType& operator[] (int k) = NULL;
 };
 
+template <class DataType>
+ostream& operator << <DataType>(ostream& s,
+	AbstractArrayClass<DataType>& ac)
+{
+	s << "[";
+	for (int i = 0; i < ac.size(); i++)
+	{
+		if (i > 0)
+		{
+			s << ',';
+		}
+		s << ac[i];
+	}
+	s << "]";
+	return s;
+} //end overloaded <<
 #endif
--- ArrayClass.h
+++ ArrayClass.h
@@ -59,4 +59,34 @@ ArrayClass<DataType>::ArrayClass(int n, const DataType& val)
 	for (int i = 0; i < n; i++)
 		paObject[i] = val;
 } //end ArrayClass constructor with n & val
+
+template <class DataType>
+ArrayClass<DataType>::ArrayClass(const ArrayClass<DataType>& ac)
+{
+	if (&ac != this) //disallow copying the object on to itself
+		copy(ac);
+} //end copy constructor
+
+template <class DataType>
+void ArrayClass<DataType>::copy(const ArrayClass<DataType>& ac)
+{
+	_size = 0; //default in case allocation fails
+	paObject = new DataType[ac._size];
+	if (paObject == NULL) throw ArrayMemoryException();
+	_size = ac._size;
+	for (int i = 0; i < _size; i++)
+	{
+		paObject[i] = ac.paObject[i];
+	}
+} //end copy method
+
+template <class DataType>
+void ArrayClass<DataType>::operator= (const ArrayClass<DataType>& ac)
+{
+	if (&ac != this) //disallow copying the object onto itself
+	{
+		if (paObject != NULL)delete[] paObject;
+		copy(ac);
+	}
+} //end overloaded =
 #endif
